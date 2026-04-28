9cbbc36ae8cc80608b871041868c455ed2bd2359
Junior <Junior@DESKTOP-VQE8F81>
2026-04-28 13:46:14 -0300

    build

--- src/app/layout/private/components/header/header.component.ts
+++ src/app/layout/private/components/header/header.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { Toolbar, ToolbarModule } from 'primeng/toolbar';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 import { Button } from 'primeng/button';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { Toolbar, ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [Toolbar, RouterLink, ToolbarModule, Button],
+  imports: [Toolbar, RouterLink, ToolbarModule, Button, SpeedDialModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  items!: MenuItem[];
+
+  constructor(private readonly router: Router) {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        icon: 'pi pi-sign-out',
+        command: () => {
+          this.deslogar();
+        },
+      },
+    ];
+  }
+
+  deslogar() {
+    sessionStorage.removeItem('despesas');
+    sessionStorage.removeItem('token');
+
+    this.router.navigate(['/login']);
+  }
+}
