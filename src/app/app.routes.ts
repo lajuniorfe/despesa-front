b758f72852853e7429c516917904d5495a3d1ed8
Junior <Junior@DESKTOP-VQE8F81>
2026-04-28 18:01:59 -0300

    Cria paginas de pagamento, desejo e relatorio e ajusta menus do header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth-guard';
 import { HomeComponent } from './features/dashboard/pages/home/home.component';
+import { DesejoComponent } from './features/desejos/components/desejo.component';
+import { LoginComponent } from './features/login/pages/login/login.component';
+import { PagamentoComponent } from './features/pagamentos/components/pagamento.component';
+import { RelatorioDespesaComponent } from './features/relatorios/relatorio-despesa/relatorio-despesa.component';
 import { PrivateLayoutComponent } from './layout/private/private-layout';
 import { PublicLayoutComponent } from './layout/public/public-layout';
-import { LoginComponent } from './features/login/pages/login/login.component';
-import { AuthGuard } from './core/guards/auth-guard';
-import { RelatorioDespesaComponent } from './features/relatorios/relatorio-despesa/relatorio-despesa.component';
 
 export const routes: Routes = [
   {
@@ -23,5 +25,17 @@ export const routes: Routes = [
     path: 'relatorios',
     component: PrivateLayoutComponent,
     children: [{ path: '', component: RelatorioDespesaComponent, canActivate: [AuthGuard] }],
+  },
+
+  {
+    path: 'pagamentos',
+    component: PrivateLayoutComponent,
+    children: [{ path: '', component: PagamentoComponent, canActivate: [AuthGuard] }],
+  },
+
+  {
+    path: 'desejos',
+    component: PrivateLayoutComponent,
+    children: [{ path: '', component: DesejoComponent, canActivate: [AuthGuard] }],
   },
 ];
