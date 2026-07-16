9f8cfa43ca26b915a2eb93515b96253c7cb3d97f
lajuniorfe <lajuniorferreira@gmail.com>
2026-07-16 09:26:42 -0300

    Ajusta url da api em produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // serverUrl: 'https://cadastro-despesa-back.onrender.com/api/',
-   serverUrl: 'api-cadastro-despesas-hbgkg7fsehcya9bu.westus3-01.azurewebsites.net/api/',
+   serverUrl: 'https://api-cadastro-despesas-hbgkg7fsehcya9bu.westus3-01.azurewebsites.net/api/',
 };
